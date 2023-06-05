3fd3c5aa651f3a84a7f453e6a2ee7bcae8c73e66
duyanhvuong2001 <102603197@student.swin.edu.au>
2023-06-06 01:23:10 +1000

    first commit

---
+++ 3DEngine/3DEngine/Window.cpp
@@ -0,0 +1,19 @@
+#include "Window.h"
+
+Window::Window()
+{
+}
+
+bool Window::Init()
+{
+	return false;
+}
+
+bool Window::Release()
+{
+	return false;
+}
+
+Window::~Window()
+{
+}
---
+++ 3DEngine/3DEngine/Window.h
@@ -0,0 +1,16 @@
+#pragma once
+class Window
+{
+public:
+	Window();
+	//Initialize window
+	bool Init();
+
+	//Release the window
+	bool Release();
+	
+	~Window();
+
+private:
+};
+
